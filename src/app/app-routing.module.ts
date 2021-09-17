31d474afe50bd8dff76655cf9642ce6ded4d5d69
you988 <umangtandel1860@gmail.com>
2021-09-17 13:44:08 +0530

    modules with lazy loading

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent},
+  { path: 'module1', loadChildren: () => import('./module1/module1.module').then(m => m.Module1Module) }, 
+  { path: 'module2', loadChildren: () => import('./module2/module2.module').then(m => m.Module2Module) }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
